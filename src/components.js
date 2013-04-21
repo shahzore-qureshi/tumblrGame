ea837c177ca43100235700f041d469e02854e175
shaq1nj <shaq1nj@aol.com>
2013-04-21 04:35:27 -0400

    Added title screen and game over screen. Added enemies and pitfalls. Bug fixes regarding enemy collision.

--- src/components.js
+++ src/components.js
@@ -11,14 +11,50 @@ Crafty.c("Obstacle",
 	}
 });
 
+Crafty.c("Enemy",
+{
+	init: function()
+	{
+		this.requires("2D, DOM, Color, Collision")
+			.color('rgb(0,67,150)')
+			.collision(
+				new Crafty.polygon([this.x, this.y - (this.h / 2)], [this.x + (this.w / 2), this.y], [this.x + this.w, this.y - (this.h / 2)]));
+				//Die only when stomped on.
+		
+		this.bind('EnterFrame', function()
+		{
+			this.x += this.dX;
+		
+			if (this.x === this.leftBoundary)
+			{
+				this.dX = 2;
+			}
+			else if (this.x === this.rightBoundary)
+			{
+				this.dX = -2;
+			}
+		});
+		
+		this.onHit("Mario", function()
+		{
+			this.destroy();
+		});
+		
+		
+		
+	}
+});
+
 Crafty.c("Mario",
 {
 	init: function()
 	{
 		this.requires("2D, DOM, Multiway, Collision, PlayerSprite, SpriteAnimation")
 			.multiway(10, {UP_ARROW: -90, RIGHT_ARROW: 0, LEFT_ARROW: -180})
-			.attr({x: 50, y: 350, w: 30, h: 30})
+			.attr({x: 50, y: 300, w: 30, h: 30})
 			.stopOnHit()
+			.stopOnPitfallDeath()
+			.stopOnEnemyHit()
 			.animate('IdleRight', 0, 0, 15)
 			.animate('RunRight', 0, 1, 20)
 			.animate('JumpRight', 0, 2, 10)
@@ -72,7 +108,30 @@ Crafty.c("Mario",
 			this.x -= this._movement.x;
 			this.y -= this._movement.y;
 		}
+	},
+	
+	stopOnPitfallDeath: function()
+	{
+		this.onHit("Pitfall", this.death);
+		return this;
+	},
+	
+	death: function()
+	{
+		Crafty.scene("Dead"); //Keep this death function separate from the one below. For some reason, Crafty confuses itself.
+	},
+	
+	stopOnEnemyHit: function()
+	{
+		this.onHit("Enemy", this.deathEnemy);
+		return this;
+	},
+	
+	deathEnemy: function()
+	{
+		Crafty.scene("Dead");
 	}
+
 });
 
 Crafty.c("NetworkPlayer",
