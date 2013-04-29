61a48dfb9e9d551a93057a55f0cb24f5970ab5af
shaq1nj <shaq1nj@aol.com>
2013-04-28 20:24:30 -0400

    Displays score at victory or loss screen. Added Goomba sprites. Added ground graphics. Enabled jump sounds. Fixed some bugs.

--- src/components.js
+++ src/components.js
@@ -1,198 +1,28 @@
 // CRAFTY - Components
 /**
+ * Mario
+ * NetworkPlayer
  * Obstacle
  * EnemyGoomba
  * EnemyBullet
  * LifeCounter
  * ScoreCounter
  * Castle
- * Mario
- * NetworkPlayer
  */
-
-var numOfLives = 5;
-var score = 0; //To increase score, kill enemies and finish the level.
 								
-Crafty.load(['assets/spriteMap.png'], function()
+Crafty.load(['assets/MarioSprite.png'], function()
 {
-	Crafty.sprite(32, 'assets/spriteMap.png', { PlayerSprite: [0, 0] }, 0 , 0);
+	Crafty.sprite(32, 'assets/MarioSprite.png', { PlayerSprite: [0, 0] }, 0 , 0);
+});
+
+Crafty.load(['assets/GoombaSprite.png'], function()
+{
+	Crafty.sprite(32, 'assets/GoombaSprite.png', { GoombaSprite: [0, 0] }, 0 , 0);
 });
 
 Crafty.audio.add("ambiance", "assets/mario.ogg");
 Crafty.audio.add("castle", "assets/castle.ogg");
 Crafty.audio.add("jump", "assets/jump.ogg");
-
-/** OBSTACLE
- * Represents a generic square obstacle of some size
- */
-Crafty.c("Obstacle",
-{
-	init: function()
-	{
-		this.requires("2D, DOM");
-	}
-});
-
-/** ENEMYGOOMBA
- * Represents an enemy that behaves like a Goomba
- * onHit - MARIO
- */
-Crafty.c("EnemyGoomba",
-{
-	init: function()
-	{
-		this.requires("2D, DOM, Color, Collision")
-			.color('rgb(55, 243, 140)')
-			.collision(
-				new Crafty.polygon(
-					[0, 30], [5, 10], [10, 5], [15, 0], [20, 5], [25, 10], [30, 30]
-					)
-				);
-				//Die only when stomped on.
-		
-		this.bind('EnterFrame', function()
-		{
-			this.x += this.dX;
-			
-			if (this.x === this.leftBoundary)
-			{
-				this.dX = 1;
-			}
-			else if (this.x === this.rightBoundary)
-			{
-				this.dX = -1;
-			}
-			
-		});
-		
-		this.onHit("Mario", function(target)
-		{			
-			if (target[0].normal.y > 0) //If Mario jumps on top of the bullet, it is dead.
-			{
-				console.log(target[0]);
-				score = score + 20;
-				Crafty("ScoreCounter").text("Score: " + score);
-				this.destroy();
-			}
-			else
-			{
-				target[0].obj.deathEnemy();
-			}
-		});
-	}
-});
-
-/** ENEMYBULLET
- * Represents an enemy Bullet Bill that flies straight through the air
- * onHit - MARIO
- */
-Crafty.c("EnemyBullet",
-{
-	init: function()
-	{
-		this.requires("2D, DOM, Image, Collision")
-			.image("assets/Bullet.png")
-			.collision(
-				new Crafty.polygon(
-					[0, 0], [40, 0], 
-					[40, 35], [0, 35]
-					)
-				);
-				//Die only when stomped on.
-		
-		this.bind('EnterFrame', function()
-		{
-			this.x += this.dX;
-		});
-		
-		this.onHit("Mario", function(target)
-		{						
-			if (target[0].normal.y > 0) //If Mario jumps on top of the bullet, it is dead.
-			{
-				score = score + 20;
-				Crafty("ScoreCounter").text("Score: " + score);
-				this.destroy();
-			}
-			else
-			{
-				target[0].obj.deathEnemy();
-			}
-		});
-	}
-});
-
-/** LIFECOUNTER
- * Test display that show how many 'lives' are left in the game
- */
-Crafty.c("LifeCounter",
-{
-	init: function()
-	{
-		this.requires("2D, DOM, Text")
-			.text("Lives: " + numOfLives)
-			.textColor("#000000")
-			.textFont(
-				{
-					font: "assets/MyriadPro.OTF",
-					weight: "bold"
-				});
-	}
-	
-});
-
-/** SCORECOUNTER
- * Text display that shows the game score
- */
-Crafty.c("ScoreCounter",
-{
-	init: function()
-	{
-		this.requires("2D, DOM, Text")
-			.text("Score: " + score)
-			.textColor("#000000")
-			.textFont(
-				{
-					font: "assets/MyriadPro.OTF",
-					weight: "bold"
-				});
-	}
-	
-});
-
-// Crafty.c("LifeCounterBg",
-// {
-// 	init: function()
-// 	{
-// 		this.requires("2D, DOM, Color")
-// 			.text("Lives: " + numOfLives)
-// 			.css({"color" : "#000000"});
-// 	}
-// 	
-// });
-
-/** CASTLE
- * Castle entity, which represents an end goal in the game
- * onHit - MARIO
- */
-Crafty.c("Castle",
-{
-	init: function()
-	{
-		this.requires("2D, DOM, Image, Collision")
-			.image("assets/Castle.png")
-			.collision(
-				new Crafty.polygon(
-					[0, 0], [32, 0],
-					[32, 32], [0, 32]
-					)
-				);
-		
-		this.onHit("Mario", function()
-		{
-			Crafty.scene("Victory");
-		});
-	}
-});
 
 /** MARIO
  * Represents the player at the local machine
@@ -201,7 +31,7 @@ Crafty.c("Mario",
 {
 	init: function()
 	{
-		this.requires("2D, DOM, Multiway, Collision, PlayerSprite, SpriteAnimation")
+		this.requires("2D, DOM, Multiway, Collision, PlayerSprite, SpriteAnimation, Persist") //Persist allows Mario to stay alive between scenes.
 			.multiway(10, {UP_ARROW: -90, RIGHT_ARROW: 0, LEFT_ARROW: -180})
 			.collision(
 				new Crafty.polygon(
@@ -209,7 +39,7 @@ Crafty.c("Mario",
 					[30, 30], [0, 30]
 					)
 				)
-			.attr({x: 50, y: 300, w: 30, h: 30})
+			.attr({x: 50, y: 300, w: 30, h: 30, numOfLives: 5, score: 0})
 			.stopOnHit()
 			.stopOnPitfallDeath()
 			.stopOnEnemyHit()
@@ -223,18 +53,27 @@ Crafty.c("Mario",
 		var animation_speed = 4;
 		this.bind('NewDirection', function(data)
 		{
-		    if (data.y < 0) {
-			if (data.x < 0)
-			    this.animate('JumpLeft', animation_speed, 1);
-			else
-			    this.animate('JumpRight', animation_speed, 1);
-		    } else {
-			if (data.x == 0)
-			    this.stop();
-			else if (data.x > 0)
-			    this.animate('RunRight', animation_speed, -1);
-			else if (data.x < 0)
-			    this.animate('RunLeft', animation_speed, -1);
+		    if (data.y < 0) 
+		    {
+				if (data.x < 0)
+				{
+			    	this.animate('JumpLeft', animation_speed, 1);
+			    	Crafty.audio.play("jump", 1);
+			    }
+				else
+				{
+			    	this.animate('JumpRight', animation_speed, 1);
+			    	Crafty.audio.play("jump", 1);
+			    }
+		    } 
+		    else 
+		    {
+				if (data.x == 0)
+					this.stop();
+				else if (data.x > 0)
+					this.animate('RunRight', animation_speed, -1);
+				else if (data.x < 0)
+					this.animate('RunLeft', animation_speed, -1);
 		    }
 		});
 	},
@@ -269,15 +108,11 @@ Crafty.c("Mario",
 	
 	deathEnemy: function(target)
 	{
-		//console.log(target);
-		numOfLives--;
-		Crafty("LifeCounter").text("Lives: " + numOfLives);
+		this.numOfLives--;
+		Crafty("LifeCounter").text("Lives: " + this.numOfLives);
 		
-		if (numOfLives === 0)
+		if (this.numOfLives === 0)
 		{
-			this.destroy();
-			numOfLives = 5; //Re-initialize for next time.
-			score = 0;
 			Crafty.scene("Dead"); //Keep this death function separate from the one below. For some reason, Crafty confuses itself.
 		}
 		else
@@ -314,15 +149,217 @@ Crafty.c("NetworkPlayer",
 			    else
 				this.animate('JumpRight', animation_speed, 1);
 			} else {
-			    if (data.coord == this.x)
+			    if (data.coordX == this.x)
 				this.stop();
-			    else if (data.coord < this.x)
+			    else if (data.coordX < this.x)
 				this.animate('RunLeft', animation_speed, 1);
-			    else if (data.coord > this.x)
+			    else if (data.coordX > this.x)
 				this.animate('RunRight', animation_speed, 1);
 			}
 			this.x = data.coordX;
 			this.y = data.coordY;
+		});
+	}
+});
+
+
+
+/** OBSTACLE
+ * Represents a generic square obstacle of some size
+ */
+Crafty.c("Obstacle",
+{
+	init: function()
+	{
+		this.requires("2D, DOM");
+	}
+});
+
+Crafty.c("Ground",
+{
+	init: function()
+	{
+		this.requires("2D, DOM, Image, Obstacle")
+			.image("assets/ground.png", "repeat");
+	}
+});
+
+/** ENEMYGOOMBA
+ * Represents an enemy that behaves like a Goomba
+ * onHit - MARIO
+ */
+Crafty.c("EnemyGoomba",
+{
+	init: function()
+	{
+		this.requires("2D, DOM, Collision, GoombaSprite, SpriteAnimation")
+			.collision([0, 30], [15, 0], [30, 30])
+			.animate('WalkLeft', 0, 0, 7)
+			.animate('WalkRight', 0, 1, 7);
+		
+		var animation_speed = 4;
+		this.bind('EnterFrame', function()
+		{
+			this.x += this.dX;
+			if (this.dX == 1)
+				this.animate('WalkRight', animation_speed, 1);
+			else if (this.dX == -1)
+				this.animate('WalkLeft', animation_speed, 1);
+			
+			if (this.x === this.leftBoundary)
+			{
+				this.dX = 1;
+			}
+			else if (this.x === this.rightBoundary)
+			{
+				this.dX = -1;
+			}
+			
+		});
+		
+		this.onHit("Mario", function(target)
+		{			
+			if (target[0].normal.y > 0) //If Mario jumps on top of the bullet, it is dead.
+			{
+				Crafty("Mario").score += 20;
+				Crafty("ScoreCounter").text("Score: " + Crafty("Mario").score);
+				this.destroy();
+			}
+			else
+			{
+				target[0].obj.deathEnemy();
+			}
+		});
+	}
+});
+
+/** ENEMYBULLET
+ * Represents an enemy Bullet Bill that flies straight through the air
+ * onHit - MARIO
+ */
+Crafty.c("EnemyBullet",
+{
+	init: function()
+	{
+		this.requires("2D, DOM, Image, Collision")
+			.image("assets/Bullet.png")
+			.collision(
+				new Crafty.polygon(
+					[0, 0], [40, 0], 
+					[40, 35], [0, 35]
+					)
+				);
+				//Die only when stomped on.
+		
+		this.bind('EnterFrame', function()
+		{
+			this.x += this.dX;
+		});
+		
+		this.onHit("Mario", function(target)
+		{					
+			if (target[0].normal.y > 0) //If Mario jumps on top of the bullet, it is dead.
+			{
+				Crafty("Mario").score += 20;
+				Crafty("ScoreCounter").text("Score: " + Crafty("Mario").score);
+				this.destroy();
+			}
+			else
+			{
+				target[0].obj.deathEnemy();
+			}
+		});
+	}
+});
+
+/** LIFECOUNTER
+ * Test display that show how many 'lives' are left in the game
+ */
+Crafty.c("LifeCounter",
+{
+	init: function()
+	{
+		this.requires("2D, DOM, Text")
+			.text("Lives: 5")
+			.textColor("#000000");
+		
+		this.css(
+				{
+					"font": "14px Arial"
+				});
+	}
+	
+});
+
+/** SCORECOUNTER
+ * Text display that shows the game score
+ */
+Crafty.c("ScoreCounter",
+{
+	init: function()
+	{
+		this.requires("2D, DOM, Text")
+			.text("Score: 0")
+			.textColor("#000000");
+		
+		this.css(
+				{
+					"font": "14px Arial",
+				});
+	}
+	
+});
+
+/** SCORECOUNTER
+ * Text display that shows the game score
+ */
+Crafty.c("DisplayScore",
+{
+	init: function()
+	{
+		this.requires("2D, DOM, Text")
+			.text("Score: 0")
+			.textColor("#000000");
+			
+		this.css(
+				{
+					"font": "36px Arial"
+				});
+	}
+	
+});
+
+// Crafty.c("LifeCounterBg",
+// {
+// 	init: function()
+// 	{
+// 		this.requires("2D, DOM, Color")
+// 			.text("Lives: " + numOfLives)
+// 			.css({"color" : "#000000"});
+// 	}
+// 	
+// });
+
+/** CASTLE
+ * Castle entity, which represents an end goal in the game
+ * onHit - MARIO
+ */
+Crafty.c("Castle",
+{
+	init: function()
+	{
+		this.requires("2D, DOM, Image, Collision")
+			.image("assets/Castle.png")
+			.collision(
+				new Crafty.polygon(
+					[0, 0], [32, 0],
+					[32, 32], [0, 32]
+					)
+				);
+		
+		this.onHit("Mario", function()
+		{
+			Crafty.scene("Victory");
 		});
 	}
 });
--- src/script.js
+++ src/script.js
@@ -48,219 +48,258 @@ function Game()
 
     //Initiate game in DOM Javascript.
     Crafty.init(900,400);
-    Crafty.scene("Title", function() {
-	Crafty.background("#FFFFFF");
-	Crafty.sprite("assets/Title.png", {
-	    title: [0, 0, 900, 400]
-	});
-	Crafty.e("2D, DOM, title").attr({x: 0});
-	this.restart_game = this.bind('KeyDown', function() {
-	    Crafty.scene('Main');
-	});
-    }, function() {
-	this.unbind('KeyDown', this.restart_game);
-    });
-
-    Crafty.scene("Dead", function() {
-	Crafty.viewport.init(900, 400);
-	Crafty.background("#FFFFFF");
-	Crafty.sprite("assets/GameOver.png", {
-	    gameOver: [0, 0, 900, 400]
-	});
-	Crafty.e("2D, DOM, gameOver").attr({x: 0});
-    	this.restart_game = this.bind('KeyDown', function() {
-	    Crafty.scene('Main');
-	});
-    }, function() {
-	this.unbind('KeyDown', this.restart_game);
-    });
-
-    Crafty.scene("Victory", function() {
-	Crafty.audio.stop("ambiance");
-	Crafty.audio.play("castle", 1);
-	Crafty.viewport.init(900, 400);
-	Crafty.background("#FFFFFF");
-	Crafty.sprite("assets/Victory.png", {
-	    victory: [0, 0, 900, 400]
-	});
-	Crafty.e("2D, DOM, victory").attr({x: 0});
-    	this.restart_game = this.bind('KeyDown', function() {
-	    Crafty.scene('Main');
-	});
-    }, function() {
-	this.unbind('KeyDown', this.restart_game);
-    });
-
-    Crafty.scene("Main", function() {
-	var bg = Crafty.e("Background, 2D, DOM, Image")
-	    .attr({w: 12000, h: 400})
-	    .image("assets/bg.png", "repeat");
-	
-	var platformBeginning = Crafty.e("Obstacle, Color")
-	    .attr({x: 0, y: 350, w: 500, h: 50})
-	    .color('rgb(0,255,150)');
-
-	Crafty.audio.play("ambiance", -1);
-
-	//Create random pitfalls via generating random bits of land.
-	for(var i = 0; i < 50; i++) 
-	{
-	    var randX = Math.floor(Math.random() * (11000 - 500 + 1) + 500);
-	    Crafty.e("Obstacle, Color")
-		.attr({x: randX, y: 350, w: 200, h: 50})
-		.color('rgb(0,255,150)');
-
-	    //Percent chance of creating enemy on platform.
-	    var createEnemyChance = 30;
-	    var result = Math.floor(Math.random() * (100 - 1 + 1) + 1);
-	    if (result <= createEnemyChance)
-	    {
-		var enemy = Crafty.e("EnemyGoomba")
-		    .attr({x: randX + 170, y: 320,
-			   leftBoundary: randX,
-			   rightBoundary: randX + 170,
-			   w: 30, h: 30, dX: -1});
-	    }
-	}
-	
-	//Spawn random enemies.
-	for (var i = 0; i < 10; i++)
-	{
-	    var randX = Math.floor(Math.random() * (11500 - 500 + 1) + 500);
-	    var randY = Math.floor(Math.random() * (300 - 35 + 1) + 35);
-	    var enemy = Crafty.e("EnemyBullet")
- 		.attr({x: randX, y: randY, w: 40, h: 35, dX: -2});
-	}
-
-	var platformEnding = Crafty.e("Obstacle, Color")
-	    .attr({x: 11500, y: 350, w: 500, h: 50})
-	    .color('rgb(0,255,150)');
-	
-	var pitfallDetector = Crafty.e("Pitfall, 2D, DOM")
-	    .attr({x: 0, y: 400, w: 12000, h: 50});		
-
-	var leftBoundary = Crafty.e("Obstacle, 2D, DOM, Color")
-	    .attr({x: -5, y: 0, w: 5, h: 400})
-	    .color('rgb(255,255,255)');
-	
-	var rightBoundary = Crafty.e("Obstacle, 2D, DOM, Color")
-	    .attr({x: 11995, y: 0, w: 5, h: 400})
-	    .color('rgb(255,255,255)');
-	
-	var castle = Crafty.e("Castle")
-	    .attr({x: 11950, y: 318, w: 32, h: 32});
-	
-	var lifeCounter = Crafty.e("LifeCounter")
-	    .attr({x: 33, y: 355, w: 100, count: 5});	
-	
-	var scoreCounter = Crafty.e("ScoreCounter")
-	    .attr({x: 32, y: 375, w: 100, count: 0});
-	
-	var mario = Crafty.e("Mario, Gravity")
-	    .bind("Moved", trackCoord)
-	//Any obstacle can stop Mario from falling.
-	    .gravity("Obstacle");
-
-	function trackCoord()
-	{
-	    lifeCounter.x = this.x;
-	    scoreCounter.x = this.x;
-	    //Push data to multiplayer server.
-	    dataRef.update({coordX: this.x, coordY: this.y});
-	}
-
-	for(var i = 0; i < imgLinks.length; i++)
-	{
-	    var randX = Math.floor(Math.random() * (11000 - 400 + 1) + 400);
-	    var randY = Math.floor(Math.random() * (150 - 100 + 1) + 150);
-	    Crafty.e("Obstacle, Image")
-		.attr({x: randX, y: randY, w:100, h:100})
-		.image(imgLinks[i]);
-	}
-
-	//Specify camera angle and view to follow Mario.
-	Crafty.viewport.follow(mario, 0, 0);
-
-	//Draw other online players using players Firebase object
-	// (not dataRef obj). Only call this once.
-	players.once('value', function(snapshot) {
-	    snapshot.forEach(function(child) {
-		childData = child.val();
-		if (childData.id !== playerID && childData.id !== undefined)
+    
+    Crafty.scene("Title", function() 
+    {
+		Crafty.background("#FFFFFF");
+		Crafty.sprite("assets/Title.png", 
 		{
-		    //console.log("current id: " + playerID);
-		    //console.log("new opponent id found: " + childData.id);
-		    networkPlayerList.push(childData.id);
-		    //console.log("Player List: " + networkPlayerList);
-		    //Use this variable to keep track of network sprite.
-		    var entityID = childData.id;
-
-		    Crafty.e("" + entityID + ", NetworkPlayer, Gravity")
-			.attr({x: childData.coordX, y: childData.coordY,
-			       w: 30, h: 30})
-		    //Added gravity so that other players land too. 
-		    //It wasn't doing it on its own.
-		    //I guess gravity coordinate changes are not tracked.
-			.gravity("Obstacle")
-		    //Update location when child data changes.
-			.bind("moveID" + entityID, changeLocation);
-		}
-	    });
-	});
-
-	//Add new players as they come.
-	players.on('child_added', function(snapshot) {
-	    var otherPlayer = snapshot.val();
-	    if (otherPlayer.id !== playerID && otherPlayer.id !== undefined)
-	    {
-		//console.log("child added id: " + otherPlayer.id);
-		networkPlayerList.push(otherPlayer.id);
-		//console.log("Player List: " + networkPlayerList);
-		var entityID = otherPlayer.id; //Use this variable to keep track of network sprite.
-		
-		Crafty.e("" + entityID + ", NetworkPlayer, Gravity")
-		    .attr({x: otherPlayer.coordX, y: otherPlayer.coordY,
-			   w: 30, h: 30})
-		//Added gravity so that other players land too. 
-		//It wasn't doing it on its own.
-		//I guess gravity coordinate changes are not tracked.
-		    .gravity("Obstacle")
-		//Update location when child data changes.
-		    .bind("moveID" + entityID, changeLocation);
-	    }
-	});
-
-	//When change is detected on server, update location of network player.
-	function changeLocation(playerObject)
-	{
-	    this.x = playerObject.coordX;
-	    this.y = playerObject.coordY;
-	}
-
-	//Update position of other players.
-	players.on('child_changed', function(snapshot) {
-	    //console.log("change detected");
-	    var otherPlayer = snapshot.val();
-	    if (otherPlayer.id !== playerID && otherPlayer.id !== undefined)
-	    {
-		//console.log("child change id: " + otherPlayer.id);
-		//Update location via event triggering.
-		//Crafty.trigger("moveID" + otherPlayer.id, otherPlayer);
-		Crafty.trigger("networkMove", otherPlayer);
-	    }
-	});
-
-	//When a network player leaves, delete him or her.	
-	players.on('child_removed', function(snapshot) {
-	    var otherPlayer = snapshot.val();
-	    if (otherPlayer.id !== playerID && otherPlayer.id !== undefined)
-	    {
-		//console.log("child removed id: " + otherPlayer.id);
-		Crafty("" + otherPlayer.id + "").each(function(i) {
-		    this.destroy();
+			title: [0, 0, 900, 400]
 		});
-	    }
+		
+		Crafty.e("2D, DOM, title").attr({x: 0});
+		
+		this.restart_game = this.bind('KeyDown', function() 
+		{
+			Crafty.scene('Main');
+		});
+		
+	}, function() 
+	{
+		this.unbind('KeyDown', this.restart_game);
 	});
+
+	Crafty.scene("Dead", function() 
+	{
+		var score = Crafty("Mario").score;
+		Crafty("Mario").destroy();
+		
+		Crafty.viewport.init(900, 400);
+		Crafty.background("#FFFFFF");
+		
+		Crafty.sprite("assets/GameOver.png", 
+		{
+			gameOver: [0, 0, 900, 400]
+		});
+		
+		Crafty.e("2D, DOM, gameOver")
+			.attr({x: 0});
+		
+		Crafty.e("DisplayScore")
+			.attr({x: 380, y: 150, w: 400, h: 200})
+			.text("Score: " + score);
+				
+		this.restart_game = this.bind('KeyDown', function() 
+		{
+			Crafty.scene('Main');
+		});
+		
+	}, function() 
+	{
+		this.unbind('KeyDown', this.restart_game);
+    });
+
+    Crafty.scene("Victory", function() 
+    {
+    	var score = Crafty("Mario").score;
+		Crafty("Mario").destroy();
+		
+		Crafty.audio.stop("ambiance");
+		Crafty.audio.play("castle", 1);
+		
+		Crafty.viewport.init(900, 400);
+		
+		Crafty.background("#FFFFFF");
+		
+		Crafty.sprite("assets/Victory.png", {
+			victory: [0, 0, 900, 400]
+		});
+		
+		Crafty.e("2D, DOM, victory")
+			.attr({x: 0});
+			
+		Crafty.e("DisplayScore")
+			.attr({x: 375, y: 150, w: 400, h: 200})
+			.text("Score: " + score);
+		
+		this.restart_game = this.bind('KeyDown', function() 
+		{
+			Crafty.scene('Main');
+		});
+		
+	}, function() 
+	{
+		this.unbind('KeyDown', this.restart_game);
+	});
+
+    Crafty.scene("Main", function() 
+    {
+		var bg = Crafty.e("Background, 2D, DOM, Image")
+			.attr({w: 12000, h: 400})
+			.image("assets/bg.png", "repeat");
+	
+		var platformBeginning = Crafty.e("Ground")
+			.attr({x: 0, y: 375, w: 500, h: 25});
+			
+		Crafty.audio.play("ambiance", -1);
+
+		//Create random pitfalls via generating random bits of land.
+		for(var i = 0; i < 50; i++) 
+		{
+			var randX = Math.floor(Math.random() * (11000 - 500 + 1) + 500);
+			Crafty.e("Ground")
+				.attr({x: randX, y: 375, w: 200, h: 25});
+				
+			//Percent chance of creating enemy on platform.
+			var createEnemyChance = 20;
+			var result = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+			if (result <= createEnemyChance)
+			{
+			var enemy = Crafty.e("EnemyGoomba")
+				.attr({x: randX + 170, y: 345,
+				   leftBoundary: randX,
+				   rightBoundary: randX + 170,
+				   w: 30, h: 30, dX: -1});
+			}
+		}
+	
+		//Spawn random enemies.
+		for (var i = 0; i < 10; i++)
+		{
+			var randX = Math.floor(Math.random() * (11500 - 500 + 1) + 500);
+			var randY = Math.floor(Math.random() * (300 - 35 + 1) + 35);
+			var enemy = Crafty.e("EnemyBullet")
+			.attr({x: randX, y: randY, w: 40, h: 35, dX: -2});
+		}
+
+		var platformEnding = Crafty.e("Ground")
+			.attr({x: 11500, y: 375, w: 500, h: 25});
+				
+		var pitfallDetector = Crafty.e("Pitfall, 2D, DOM")
+			.attr({x: 0, y: 400, w: 12000, h: 50});		
+
+		var leftBoundary = Crafty.e("Obstacle, 2D, DOM, Color")
+			.attr({x: -5, y: 0, w: 5, h: 400})
+			.color('rgb(255,255,255)');
+	
+		var rightBoundary = Crafty.e("Obstacle, 2D, DOM, Color")
+			.attr({x: 11995, y: 0, w: 5, h: 400})
+			.color('rgb(255,255,255)');
+	
+		var castle = Crafty.e("Castle")
+			.attr({x: 11950, y: 318, w: 32, h: 32});
+	
+		var lifeCounter = Crafty.e("LifeCounter")
+			.attr({x: 15, y: 383, w: 100});	
+	
+		var scoreCounter = Crafty.e("ScoreCounter")
+			.attr({x: 70, y: 383, w: 100});
+		
+		var mario = Crafty.e("Mario, Gravity")
+			.bind("Moved", trackCoord)
+		//Any obstacle can stop Mario from falling.
+			.gravity("Obstacle");
+
+		function trackCoord()
+		{
+			lifeCounter.x = this.x - 35;
+			scoreCounter.x = this.x + 20;
+			//Push data to multiplayer server.
+			dataRef.update({coordX: this.x, coordY: this.y});
+		}
+
+		for(var i = 0; i < imgLinks.length; i++)
+		{
+			var randX = Math.floor(Math.random() * (11000 - 400 + 1) + 400);
+			var randY = Math.floor(Math.random() * (150 - 100 + 1) + 150);
+			Crafty.e("Obstacle, Image")
+			.attr({x: randX, y: randY, w:100, h:100})
+			.image(imgLinks[i]);
+		}
+
+		//Specify camera angle and view to follow Mario.
+		Crafty.viewport.follow(mario, 0, 0);
+
+		//Draw other online players using players Firebase object
+		// (not dataRef obj). Only call this once.
+		players.once('value', function(snapshot) {
+			snapshot.forEach(function(child) {
+			childData = child.val();
+			if (childData.id !== playerID && childData.id !== undefined)
+			{
+				//console.log("current id: " + playerID);
+				//console.log("new opponent id found: " + childData.id);
+				networkPlayerList.push(childData.id);
+				//console.log("Player List: " + networkPlayerList);
+				//Use this variable to keep track of network sprite.
+				var entityID = childData.id;
+
+				Crafty.e("" + entityID + ", NetworkPlayer, Gravity")
+				.attr({x: childData.coordX, y: childData.coordY,
+					   w: 30, h: 30})
+				//Added gravity so that other players land too. 
+				//It wasn't doing it on its own.
+				//I guess gravity coordinate changes are not tracked.
+				.gravity("Obstacle")
+				//Update location when child data changes.
+				.bind("moveID" + entityID, changeLocation);
+			}
+			});
+		});
+
+		//Add new players as they come.
+		players.on('child_added', function(snapshot) {
+			var otherPlayer = snapshot.val();
+			if (otherPlayer.id !== playerID && otherPlayer.id !== undefined)
+			{
+			//console.log("child added id: " + otherPlayer.id);
+			networkPlayerList.push(otherPlayer.id);
+			//console.log("Player List: " + networkPlayerList);
+			var entityID = otherPlayer.id; //Use this variable to keep track of network sprite.
+		
+			Crafty.e("" + entityID + ", NetworkPlayer, Gravity")
+				.attr({x: otherPlayer.coordX, y: otherPlayer.coordY,
+				   w: 30, h: 30})
+			//Added gravity so that other players land too. 
+			//It wasn't doing it on its own.
+			//I guess gravity coordinate changes are not tracked.
+				.gravity("Obstacle")
+			//Update location when child data changes.
+				.bind("moveID" + entityID, changeLocation);
+			}
+		});
+
+		//When change is detected on server, update location of network player.
+		function changeLocation(playerObject)
+		{
+			this.x = playerObject.coordX;
+			this.y = playerObject.coordY;
+		}
+
+		//Update position of other players.
+		players.on('child_changed', function(snapshot) {
+			//console.log("change detected");
+			var otherPlayer = snapshot.val();
+			if (otherPlayer.id !== playerID && otherPlayer.id !== undefined)
+			{
+			//console.log("child change id: " + otherPlayer.id);
+			//Update location via event triggering.
+			//Crafty.trigger("moveID" + otherPlayer.id, otherPlayer);
+			Crafty.trigger("networkMove", otherPlayer);
+			}
+		});
+
+		//When a network player leaves, delete him or her.	
+		players.on('child_removed', function(snapshot) {
+			var otherPlayer = snapshot.val();
+			if (otherPlayer.id !== playerID && otherPlayer.id !== undefined)
+			{
+			//console.log("child removed id: " + otherPlayer.id);
+			Crafty("" + otherPlayer.id + "").each(function(i) {
+				this.destroy();
+			});
+			}
+		});
     });
 
     Crafty.scene("Title");
